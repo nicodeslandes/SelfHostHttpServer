285cc550c4456d11cbfe403041c1b6017f7426c3
Nicolas Deslandes <ndeslandes@live.fr>
2024-01-31 21:59:18 +0000

    Add project files.

---
+++ SelfHostHttpServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SelfHostHttpServer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Yo! I can process messages!");
+        var server = new WebServer();
+        server.Start();
+
+        Console.ReadLine();
+        server.Stop();
+        Console.WriteLine("Bye");
+    }
+}
---
+++ SelfHostHttpServer/WebServer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
+using Owin;
+
+namespace SelfHostHttpServer;
+
+public class WebServer
+{
+    private IDisposable _webapp;
+
+    public void Start()
+    {
+        _webapp = WebApp.Start<Startup>("http://localhost:8080");
+    }
+
+    public void Stop()
+    {
+        _webapp?.Dispose();
+    }
+}
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.Run(context =>
+        {
+            string t = DateTime.Now.Millisecond.ToString();
+            return context.Response.WriteAsync(t + " Production OWIN App");
+        });
+    }
+}
